64928534a0b6da9f45638a3e5d45ba55b038749f
Aryan Vishwakarma <2402010089@cgcjhanjeri.in>
2026-04-14 04:22:27 +0000

    fixes

--- project/src/app/products-component/products-component.ts
+++ project/src/app/products-component/products-component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { GraphqlService } from '../services/graphql';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { ProductService } from '../services/product-service';
+import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'app-products-component',
   standalone: true,
@@ -10,34 +10,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './products-component.html',
   styleUrl: './products-component.css',
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  productTitle = ''
+  productPrice = 0
 
-  products: any[] = [];   // ✅ FIXED
-
-  productTitle = '';      // (optional if adding product)
-  productPrice: number | null = null;
-  constructor(private gql: GraphqlService) { }
-
-  async ngOnInit() {
-    this.products = await this.gql.listProducts(); // ✅ FIXED
+  addProduct(){
+    console.log(this.productTitle, this.productPrice)
+  }
+  constructor(private productService: ProductService, private cdr: ChangeDetectorRef) {}
+  products: any[] = []
+  ngOnInit(){
+    this.productService.getProducts().subscribe((data:any) => {
+      this.products = data.products
+      this.cdr.detectChanges()
+      console.log(data)
+    })
   }
 
-  async addProduct() {
-  // 🔥 validation
-  if (!this.productTitle || this.productPrice === null) {
-    alert("Please enter title and price");
-    return;
-  }
-  console.log("Before API:", this.productTitle, this.productPrice);
-
-  const newProduct = await this.gql.createProduct(
-    this.productTitle,
-    this.productPrice
-  );
-  console.log(newProduct)
-  this.products.push(newProduct);
-  // reset
-  this.productTitle = '';
-  this.productPrice = null;
-}
 }
